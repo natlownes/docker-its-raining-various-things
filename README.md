3d24eb1c9ffd17e69b76086ed458046e726b809e
nat <nat.lownes@gmail.com>
2015-07-25 11:15:26 -0400

    2.0.0 allow stdin

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # its-raining-various-things
 
+Will take a text file or a phrase and give you an mp3 of it raining that thing.
+
+with a file named mobydick.txt:
 ```
-docker run narf/its-raining-various-things "horse meat" > out.mp3
+docker run -i narf/its-raining-various-things < mobydick.txt > mobydick.mp3
+```
+
+with a phrase:
+```
+echo "horse meat" | docker run -i narf/its-raining-various-things > horsemeat.mp3
 ```
--- bin/run.sh
+++ bin/run.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 
+text=$(mktemp --suffix=.txt)
 speech=$(mktemp)
 outfile="$(mktemp --suffix=.mp3)"
 audio_dir="/media"
 
-echo "${1}" \
-  | text2wave -scale 5 -o ${speech} && \
-  ffmpeg \
-    -y \
-    -i "${audio_dir}/01.wav" \
-    -i ${speech} \
-    -i "${audio_dir}/02.wav" \
-    -i ${speech} \
-    -i "${audio_dir}/03.wav" \
-    -filter_complex 'concat=5:v=0:a=1' \
-    ${outfile} && \
-    cat ${outfile}
+cat /dev/fd/0>${text}
+
+text2wave -scale 5 -o ${speech} ${text} && \
+ffmpeg \
+  -y \
+  -i "${audio_dir}/01.wav" \
+  -i ${speech} \
+  -i "${audio_dir}/02.wav" \
+  -i ${speech} \
+  -i "${audio_dir}/03.wav" \
+  -filter_complex 'concat=5:v=0:a=1' \
+  ${outfile} && \
+  cat ${outfile}
