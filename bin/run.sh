fe2f9a217d30dfdfe6915a306c9ba1d95bfcf766
nat <nat.lownes@gmail.com>
2017-09-18 11:42:29 -0400

    adding Iverson
    (should expand run.sh to zip up media in a directory with
    leading/trailing phrase)

--- bin/run.sh
+++ bin/run.sh
@@ -2,7 +2,7 @@
 
 text=$(mktemp --suffix=.txt)
 speech=$(mktemp)
-audio_dir="/media"
+audio_dir="/media/iverson"
 
 function cleanup {
   rm ${text}
@@ -13,7 +13,8 @@ trap cleanup EXIT
 
 cat /dev/fd/0>${text}
 
-text2wave -scale 5 -o ${speech} ${text} && \
+text2wave -scale 2 -o ${speech} ${text} && \
+#text2wave -o ${speech} ${text} && \
 ffmpeg \
   -y \
   -i "${audio_dir}/01.wav" \
@@ -21,6 +22,31 @@ ffmpeg \
   -i "${audio_dir}/02.wav" \
   -i ${speech} \
   -i "${audio_dir}/03.wav" \
-  -filter_complex 'concat=5:v=0:a=1' \
+  -i ${speech} \
+  -i "${audio_dir}/04.wav" \
+  -i ${speech} \
+  -i "${audio_dir}/05.wav" \
+  -i ${speech} \
+  -i "${audio_dir}/06.wav" \
+  -i ${speech} \
+  -i "${audio_dir}/07.wav" \
+  -i ${speech} \
+  -i "${audio_dir}/08.wav" \
+  -i ${speech} \
+  -i "${audio_dir}/09.wav" \
+  -i ${speech} \
+  -i "${audio_dir}/10.wav" \
+  -i ${speech} \
+  -i "${audio_dir}/11.wav" \
+  -i ${speech} \
+  -i "${audio_dir}/12.wav" \
+  -i ${speech} \
+  -i "${audio_dir}/13.wav" \
+  -i ${speech} \
+  -i "${audio_dir}/14.wav" \
+  -i ${speech} \
+  -i "${audio_dir}/15.wav" \
+  -i ${speech} \
+  -filter_complex 'concat=30:v=0:a=1' \
   -f mp3 \
   pipe:1
