4ded56859dcfe53d077f0a7a8ec5eef5e7d2686d
nat <nat.lownes@gmail.com>
2017-09-18 11:42:29 -0400

    adding Iverson
    (should expand run.sh to zip up media in a directory with
    leading/trailing phrase)

--- README.md
+++ README.md
@@ -1,13 +1,24 @@
 # its-raining-various-things
 
+couple of themes now:
+
+* raining
+* iverson
+
 Will take a text file or a phrase and give you an mp3 of it raining that thing.
 
 with a file named mobydick.txt:
 ```
-docker run -i narf/its-raining-various-things < mobydick.txt > mobydick.mp3
+docker run -i narf/its-raining-various-things -- raining < mobydick.txt > mobydick.mp3
 ```
 
 [with a phrase](https://s3.amazonaws.com/nat.vistarmedia.com/audio/horsemeat.mp3):
 ```
-echo "horse meat" | docker run -i narf/its-raining-various-things > horsemeat.mp3
+echo "horse meat" | docker run -i narf/its-raining-various-things -- raining > horsemeat.mp3
+```
+
+OR
+
+```
+echo "practice" | docker run -i narf/its-raining-various-things -- iverson > horsemeat.mp3
 ```
--- bin/run.sh
+++ bin/run.sh
@@ -2,7 +2,9 @@
 
 text=$(mktemp --suffix=.txt)
 speech=$(mktemp)
-audio_dir="/media"
+theme=$2
+audio_dir="/media/${theme}"
+scale=5
 
 function cleanup {
   rm ${text}
@@ -13,14 +15,57 @@ trap cleanup EXIT
 
 cat /dev/fd/0>${text}
 
-text2wave -scale 5 -o ${speech} ${text} && \
-ffmpeg \
-  -y \
-  -i "${audio_dir}/01.wav" \
-  -i ${speech} \
-  -i "${audio_dir}/02.wav" \
-  -i ${speech} \
-  -i "${audio_dir}/03.wav" \
-  -filter_complex 'concat=5:v=0:a=1' \
-  -f mp3 \
-  pipe:1
+function raining {
+  text2wave -scale ${scale} -o ${speech} ${text} && \
+  ffmpeg \
+    -y \
+    -i "${audio_dir}/01.wav" \
+    -i ${speech} \
+    -i "${audio_dir}/02.wav" \
+    -i ${speech} \
+    -i "${audio_dir}/03.wav" \
+    -filter_complex 'concat=5:v=0:a=1' \
+    -f mp3 \
+    pipe:1
+}
+
+function iverson {
+  text2wave -scale 2 -o ${speech} ${text} && \
+  ffmpeg \
+    -y \
+    -i "${audio_dir}/01.wav" \
+    -i ${speech} \
+    -i "${audio_dir}/02.wav" \
+    -i ${speech} \
+    -i "${audio_dir}/03.wav" \
+    -i ${speech} \
+    -i "${audio_dir}/04.wav" \
+    -i ${speech} \
+    -i "${audio_dir}/05.wav" \
+    -i ${speech} \
+    -i "${audio_dir}/06.wav" \
+    -i ${speech} \
+    -i "${audio_dir}/07.wav" \
+    -i ${speech} \
+    -i "${audio_dir}/08.wav" \
+    -i ${speech} \
+    -i "${audio_dir}/09.wav" \
+    -i ${speech} \
+    -i "${audio_dir}/10.wav" \
+    -i ${speech} \
+    -i "${audio_dir}/11.wav" \
+    -i ${speech} \
+    -i "${audio_dir}/12.wav" \
+    -i ${speech} \
+    -i "${audio_dir}/13.wav" \
+    -i ${speech} \
+    -i "${audio_dir}/14.wav" \
+    -i ${speech} \
+    -i "${audio_dir}/15.wav" \
+    -i ${speech} \
+    -filter_complex 'concat=30:v=0:a=1' \
+    -f mp3 \
+    pipe:1
+}
+
+${theme}
